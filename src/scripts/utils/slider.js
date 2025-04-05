222ab7cba03dc67be3d439c5fc7ff5c2d1c58b81
antonkuchmasov <antek.ukraine@gmail.com>
2025-04-05 14:27:09 +0300

    implemented Slider on main page

---
+++ src/scripts/utils/slider.js
@@ -0,0 +1,43 @@
+const slider = document.querySelector(".slider");
+const slides = document.querySelectorAll(".slider img");
+const navButtons = document.querySelectorAll(".slider-nav button");
+let currentSlide = 0;
+let autoSlideInterval;
+
+function goToSlide(index) {
+    slider.scrollTo({
+      left: slides[index].offsetLeft,
+      behavior: 'smooth',
+    });
+
+    navButtons.forEach((btn) => btn.classList.remove('active'));
+    navButtons[index].classList.add('active');
+    currentSlide = index;
+  }
+
+  function startAutoSlide() {
+    autoSlideInterval = setInterval(() => {
+      currentSlide = (currentSlide + 1) % slides.length; 
+      goToSlide(currentSlide);
+    }, 2000); 
+  }
+
+  function stopAutoSlide() {
+    clearInterval(autoSlideInterval);
+  }
+
+  navButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+      stopAutoSlide(); 
+      goToSlide(index);
+    });
+  });
+
+  startAutoSlide();
+
+  navButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      stopAutoSlide();
+      setTimeout(startAutoSlide, 5000); 
+    });
+  });
