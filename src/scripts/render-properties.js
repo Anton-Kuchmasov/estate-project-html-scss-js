56b9575f430a1b6279ff25af472ca9ca5110016c
antonkuchmasov <antek.ukraine@gmail.com>
2025-04-05 19:01:17 +0300

    added search & reset params functional, improved UI styles

--- src/scripts/render-properties.js
+++ src/scripts/render-properties.js
@@ -1,34 +1,45 @@
-import scaleIcon from "/public/images/icons/icon-scale.svg";
-import bedIcon from "../../public/images/icons/icon-bed.svg";
-import bathroomIcon from "/public/images/icons/icon-bathroom.svg";
-import estateCard1 from "/public/images/photos/estate-card-1.png";
-import estateCard2 from "/public/images/photos/estate-card-2.png";
-import estateCard3 from "/public/images/photos/estate-card-3.png";
-import estateCard4 from "/public/images/photos/estate-card-4.png";
-import estateCard5 from "/public/images/photos/estate-card-5.png";
-import estateCard6 from "/public/images/photos/estate-card-6.png";
-
-const imageMap = {
-  "/public/images/photos/estate-card-1.png": estateCard1,
-  "/public/images/photos/estate-card-2.png": estateCard2,
-  "/public/images/photos/estate-card-3.png": estateCard3,
-  "/public/images/photos/estate-card-4.png": estateCard4,
-  "/public/images/photos/estate-card-5.png": estateCard5,
-  "/public/images/photos/estate-card-6.png": estateCard6,
-};
 import properties from "/public/api/properties.json";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const container = document.querySelector(".popular__container");
-    console.log(bedIcon);
+  const container = document.querySelector(".properties__container");
   try {
-    const updatedProperties = properties.map((property) => ({
-      ...property,
-      img: imageMap[property.img] || property.img,
-    }));
-    renderProperties(updatedProperties);
+    renderProperties(properties);
   } catch (error) {
     console.error("Произошла ошибка:", error);
+  }
+  const searchButton = document.querySelector("#search-button");
+  const resetButton = document.querySelector("#reset-button");
+  const searchForm = document.querySelector(".hero__searchbar");
+  const searchInput = searchForm.elements["search"];
+
+  resetButton.disabled = true;
+  searchButton.disabled = true;
+
+  searchForm.addEventListener("keydown", (ev) => {
+    if (ev.key === "Enter") {
+      ev.preventDefault();
+      handleSearch();
+    }
+  });
+  searchButton.addEventListener("click", handleSearch);
+  resetButton.addEventListener("click", resetSearchParams);
+  searchInput.addEventListener("input", () => {
+    resetButton.disabled = searchInput.value.trim() === "";
+    searchButton.disabled = searchInput.value.trim() === "";
+  });
+
+  function handleSearch() {
+    const searchQuery = searchInput.value;
+    const propertiesToRender = properties.filter((prop) =>
+      prop.title.toLowerCase().includes(searchQuery)
+    );
+    renderProperties(propertiesToRender);
+  }
+
+  function resetSearchParams() {
+    searchInput.value = "";
+    renderProperties(properties);
+    resetButton.disabled = true;
   }
 
   function renderProperties(properties) {
@@ -36,24 +47,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
     properties.forEach((property) => {
       const card = document.createElement("article");
-      card.className = "popular__card";
+      card.className = "properties__card";
       card.innerHTML = `
-          <div class="card card__container">
+          <div class="card__container">
             <img src="${property.img}" alt="Estate Photo" />
             <div class="card__delimiter"></div>
-            <p class="card__price">${property.price}</p>
-            <p class="card__description">${property.description}</p>
+            <p class="card__price">$${property.price.toLocaleString(
+              "en-US"
+            )}</p>
+            <p class="card__description">${property.title}</p>
             <div class="card__props">
               <div class="card__prop">
-                <img src="${scaleIcon.default}" alt="Scale" />
-                <span class="card__prop--text">${property.size}</span>
+                <img src="images/icons/icon-scale.svg" alt="Scale" />
+                <span class="card__prop--text">${property.area}</span>
               </div>
               <div class="card__prop">
-                <img src="${bedIcon}" alt="Bedrooms" />
+                <img src="images/icons/icon-bed.svg" alt="Bedrooms" />
                 <span class="card__prop--text">${property.bedrooms}</span>
               </div>
               <div class="card__prop">
-                <img src="${bathroomIcon}" alt="Bathrooms" />
+                <img src="images/icons/icon-bathroom.svg" alt="Bathrooms" />
                 <span class="card__prop--text">${property.bathrooms}</span>
               </div>
             </div>
